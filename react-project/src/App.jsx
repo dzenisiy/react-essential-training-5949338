67fb291c920ed425d64d138b7aed60819cddab9f
Yaraslau Dzenisiuk <cylinder.y@gmail.com>
2025-10-15 15:08:57 +0200

    feat: trying JSX

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -1,10 +1,13 @@
 import {useState} from 'react';
 import './App.css';
 
+let lang = "React"
+let moon = "🌙"
+
 function App() {
   const [count, setCount] = useState(0);
 
-  return <h1>hello react</h1>;
+  return <h1>hello react {lang.toUpperCase()} {moon}</h1>;
 }
 
 export default App;
