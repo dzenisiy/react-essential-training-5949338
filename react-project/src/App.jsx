620f9ed633c5c042b352427a5cf5389a4764fc9d
Yaraslau Dzenisiuk <cylinder.y@gmail.com>
2025-10-15 15:19:28 +0200

    feat: adding header component

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -1,13 +1,27 @@
 import {useState} from 'react';
 import './App.css';
 
-let lang = "React"
-let moon = "🌙"
+let lang = 'React';
+let moon = '🌙';
+
+function Header() {
+  return (
+      <header>
+        <h1>Jarek's kitchen</h1>
+      </header>
+  );
+}
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <h1>hello react {lang.toUpperCase()} {moon}</h1>;
+  return (<>
+        <Header/>
+        <main>
+          <h2>some content</h2>
+        </main>
+      </>
+  );
 }
 
 export default App;
