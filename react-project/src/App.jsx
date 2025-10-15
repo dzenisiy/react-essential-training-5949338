c165bd3ae6793914d92c903e287e3b344d99ba0c
Yaraslau Dzenisiuk <cylinder.y@gmail.com>
2025-10-15 15:06:40 +0200

    fix: add formating

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -2,9 +2,9 @@ import {useState} from 'react';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return <h1>hello react</h1>;
 }
 
-export default App
+export default App;
