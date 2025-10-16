eebef9bb9dfed710f816d6df0551110870024a91
Yaraslau Dzenisiuk <cylinder.y@gmail.com>
2025-10-16 14:57:11 +0200

    feat: passing data as props

--- next-react-project/src/app/hotels/page.js
+++ next-react-project/src/app/hotels/page.js
@@ -1,9 +1,29 @@
-export default function Page() {
+async function getData() {
+  const res = await fetch('https://snowtooth-hotel-api.fly.dev');
+  return res.json();
+}
+
+function HotelBLock({name, capacity}) {
+  return (
+      <div>
+        <h2>{name}</h2>
+        <p>{capacity}</p>
+      </div>
+  );
+}
+
+export default async function Page() {
+  const res = await getData();
   return (
       <main>
         <div>
           <h1>Hotel Details</h1>
+          <div>
+            {res.map((hotel) => (
+              <HotelBLock hotel={hotel.id} name={hotel.name} capacity={hotel.capacity} />
+              ))}
+          </div>
         </div>
       </main>
-  )
+  );
 }
